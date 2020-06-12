9435c9e50bea3944e9b75b6a1eee64a9131438ca
Jun Chen <junchen07@gmail.com>
2020-06-12 14:20:52 -0400

    update v2
    update to allow string input and misc. characters in text

--- cypherText/cypherTextv2.sh
+++ cypherText/cypherTextv2.sh
@@ -1,20 +1,14 @@
 #! /bin/bash
 
-filename=$1
+filename=$@
 cap=ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ
 low=abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
-
-if [ $2 ]
-then
-echo "Only single word or text file allowed. Try Again."
-exit
-fi
 
 if [ ! -f "$filename" ]
 then
  for ((i=1; i<=26; i++))
  do
-  str=$(echo $1 | tr [:upper:] [${cap:${i}:26}] | tr [:lower:] [${low:${i}:26}])
+  str=$(echo $@ | tr [:upper:] [${cap:${i}:26}] | tr [:lower:] [${low:${i}:26}])
   printf "Rot$(( $i - 1 )): " 
   echo ${str}
  done
@@ -29,7 +23,7 @@ while read -n1 c; do
    then
    printf $(echo $c | tr "${cap:0:26}" "${cap:${i}:26}" | tr "${low:0:26}" "${low:${i}:26}")
    else
-   printf " "
+   printf "$c"
    fi
 done <$filename 
 echo ""
